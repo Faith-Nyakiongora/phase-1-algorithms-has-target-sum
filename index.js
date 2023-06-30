b98667384fe048a726c5f28b41651a5b4d22a8af
+ <+>
2023-06-30 15:32:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const complements = new Set();
+
+  for (let num of array) {
+    if (complements.has(num)){
+      return true;
+    }
+
+    const complement = target -num;
+    complements.add(complement)
+  }
+  
+  return false;
 }
 
-/* 
+
+  /* 
   Write the Big O time complexity of your function here
-*/
+  The size of rhe input array is n. thus the time complexity is 0(n).
+  there is ans iteration through each number in the array once, performing constant-time operations for each number. 
+  */
 
 /* 
   Add your pseudocode here
+  Create an empty set called 'complements'
+  For each number 'num' in the input array:
+      If 'num' is in the 'complements' set:
+          - Return true (pair found)
+     Calculate the complement as 'target - num'
+     Add the complement to the 'complements' set
 */
 
 /*
